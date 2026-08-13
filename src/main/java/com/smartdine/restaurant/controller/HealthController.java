01301db6f46f7e21b34a6d729bc0ab5980a32a4d
Sourav <sourav280106@gmail.com>
2026-08-13 16:27:54 +0530

    update

---
+++ src/main/java/com/smartdine/restaurant/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.smartdine.restaurant.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "SmartDine is running!";
+    }
+}
